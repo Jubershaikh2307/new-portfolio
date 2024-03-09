dce0ed3823d3c213a98a9653e7413ee3186e96f9
Juber Shaikh <jubershaikh2307@gmail.com>
2024-03-09 22:46:37 +0530

    maintain the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import './App.css'
 
-import reactLogo from './assets/react.svg'
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className='w-full h-1/2 bg-black'>
